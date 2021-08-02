07d2487e53cc8c0659322e13eca337131573a9ab
jasonthorne <jasonthorne@live.ie>
2021-08-02 23:57:09 +0100

    AirForceCellController

--- workspace/PlanesData/src/controller/EventCellController.java
+++ workspace/PlanesData/src/controller/EventCellController.java
@@ -3,17 +3,13 @@ package controller;
 import com.jfoenix.controls.JFXListCell;
 
 import javafx.fxml.FXML;
-import javafx.geometry.Pos;
-import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.HBox;
 import model.Event;
 
 public class EventCellController extends JFXListCell<Event> {
 
     //root fxml element:
-    //@FXML private AnchorPane rootAP;
-    @FXML private HBox rootHB;
+    @FXML private AnchorPane rootAP;
     
     //update cell with event name:
 	@Override  
@@ -27,20 +23,6 @@ public class EventCellController extends JFXListCell<Event> {
   	    } else {
   	    	//set text with event name:
 			setText(event.getName());
-			
-			
-			// Create centered Label
-           /* Label label = new Label(event.getName());
-            label.setAlignment(Pos.CENTER);
-
-            rootHB.getChildren().setAll(label);*/
-            //setGraphic(rootHB);
-            
-            
-			
-			
-			
-			
         }
     }
 }
