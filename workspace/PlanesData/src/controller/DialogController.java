34906caa482409132f6f6a181ace4f30421e5841
jasonthorne <jasonthorne@live.ie>
2021-08-12 16:22:09 +0100

    bug loading dialog in jar :(

--- workspace/PlanesData/src/controller/DialogController.java
+++ workspace/PlanesData/src/controller/DialogController.java
@@ -1,11 +1,14 @@
 package controller;
 
+import java.io.IOException;
 import java.util.List;
 
 import com.jfoenix.controls.JFXDialog;
 import com.jfoenix.controls.JFXDialogLayout;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
@@ -23,12 +26,42 @@ public class DialogController implements Rootable {
     @FXML
     void initialize() {
     	rootDialog.setContent(contentDL); //set contentDL as content
+    	
     }
     
-    Parent root = Rootable.getRoot(this, "/view/dialog.fxml"); //root
+    ////Parent root = Rootable.getRoot(this, "/view/dialog.fxml"); //root
+   Parent root = Rootable.getRoot(this, "/view/dialog.fxml"); //root
+    
+    //FXMLLoader loader;
+  // Parent root; 
+	////////
     
     //constructor:
     DialogController(List<String>planeNames, String planeType){
+    	
+    	
+       // loader.setController(rootable); //set this class as the controller
+       // Parent root = loader.setController(rootable);
+    	
+        
+		try {
+			
+			
+			/*
+			loader = FXMLLoader.load(getClass().getResource("/view/dialog.fxml"));
+			 root = loader.load();
+			loader.setController(root);*/
+			/*
+			FXMLLoader fxmlLoader = new FXMLLoader("/view/dialog.fxml");
+			fxmlLoader.setRoot(rootDialog);
+			fxmlLoader.setController(this);
+			fxmlLoader.load();*/
+	       
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}
+        
     	
     	//set heading text with plane type and amount:
     	headingText.setText(String.valueOf(planeNames.size()) + " " + planeType);
@@ -40,5 +73,21 @@ public class DialogController implements Rootable {
    	}
     
     //show dialog on given stack pane:
-    void show(StackPane stackPane) { rootDialog.show(stackPane); }
+    void show(StackPane stackPane) { 
+    	System.out.println("yo");
+    	
+    	rootDialog.show(stackPane); 
+    	/*
+    	new Thread(new Runnable() {
+    	    @Override public void run() {
+		    	Platform.runLater(new Runnable() {
+		            @Override public void run() {
+		            	rootDialog.show(stackPane); 
+		            }
+		        });
+    	    }
+	    }).start();	*/	
+    	
+    	
+    	}
 }
