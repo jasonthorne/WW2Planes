47f9db8ed8d07a31608b97d2f4e94c9c20b1cfc2
jasonthorne <jasonthorne@live.ie>
2021-09-08 00:13:57 +0100

    stage close event

--- workspace/WW2Planes/src/application/Main.java
+++ workspace/WW2Planes/src/application/Main.java
@@ -2,6 +2,7 @@ package application;
 
 import controller.PreloaderController;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
@@ -17,6 +18,19 @@ public class Main extends Application {
 		//set stage's icon and title:
 		stage.getIcons().add(new Image(getClass().getResourceAsStream("/img/spitfire.png")));
 		stage.setTitle("WW2 Planes");
+		
+		//--------------------------------------------
+		
+		stage.setOnCloseRequest(event ->{
+			Platform.runLater(new Runnable() {
+				@Override
+				public void run() {
+					System.out.println("closing stage"); //+++serialize data ++++
+					//https://www.javatpoint.com/serialization-in-java
+				}
+			});
+		});
+		//----------------------------------------
 		
 		//create preloader controller with stage:
 		final PreloaderController preloaderCtrlr = new PreloaderController(stage);
