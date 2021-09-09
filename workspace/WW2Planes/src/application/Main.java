7cf918380d085a8c4dad144c84877bd3756711b6
jasonthorne <jasonthorne@live.ie>
2021-09-09 17:05:24 +0100

    cleanup

--- workspace/WW2Planes/src/application/Main.java
+++ workspace/WW2Planes/src/application/Main.java
@@ -2,7 +2,6 @@ package application;
 
 import controller.PreloaderController;
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
@@ -18,19 +17,6 @@ public class Main extends Application {
 		//set stage's icon and title:
 		stage.getIcons().add(new Image(getClass().getResourceAsStream("/img/spitfire.png")));
 		stage.setTitle("WW2 Planes");
-		
-		//--------------------------------------------
-		
-		stage.setOnCloseRequest(event ->{
-			Platform.runLater(new Runnable() {
-				@Override
-				public void run() {
-					System.out.println("closing stage"); //+++serialize data ++++
-					//https://www.javatpoint.com/serialization-in-java
-				}
-			});
-		});
-		//----------------------------------------
 		
 		//create preloader controller with stage:
 		final PreloaderController preloaderCtrlr = new PreloaderController(stage);
